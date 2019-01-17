4b67259685d2a266d83941f6451a7d2329e608e6
reinoptland <reinoptland@gmail.com>
2019-01-17 16:49:26 +0100

    OldestSoFar passes test

--- oldestSoFar.js
+++ oldestSoFar.js
@@ -1,5 +1,18 @@
 function oldestSoFar(people){
-    return {}
+    // Input
+    // people [{ name: 'Rein', age: 31 }, ...]
+
+    return people.reduce((oldestPersonSoFar, person) => {
+        console.log(oldestPersonSoFar, person)
+        if(oldestPersonSoFar.age > person.age){
+            return oldestPersonSoFar
+        }
+
+        return person
+    })
+
+    // Output
+    // { name: 'Rein', age: 31 }
 }
 
 module.exports.oldestSoFar = oldestSoFar
